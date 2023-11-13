252c3c86a46728118aa48c715e7d1a60c008910f
rsveinson <rsveinson@mts.net>
2023-11-13 10:33:24 -0600

    some commentiung changes

--- CS20SNormalizingExample001.java
+++ CS20SNormalizingExample001.java
@@ -29,18 +29,18 @@ public class CS20SNormalizingExample001{
         String strin = "";          // intput
         String strout = "";         // used for formatted output
         
-        // elephat weight variables
+        // elephant weight variables
         // weight of elephant 1
         int e1Tonnes = 0;
         int e1Kilos = 0;
         int e1Grams = 0;
-        int e1TotalGrams = 0;
+        int e1TotalGrams = 0;       // normalized weight
         
         // weight of elephant 2
         int e2Tonnes = 0;
         int e2Kilos = 0;
         int e2Grams = 0;
-        int e2TotalGrams = 0;
+        int e2TotalGrams = 0;       // normalized weight
         
         // total weight
         int totalWeightInGrams = 0;
@@ -78,7 +78,7 @@ public class CS20SNormalizingExample001{
         System.out.println(e1Tonnes + ":" + e1Kilos + ":" + e1Grams);
         System.out.println();
         
-        // get weight of elephant 2
+        // // get weight of elephant 2
         prompt = "Enter the weight of elephant 2 in tonnes kilos grams";
         System.out.println(prompt);
         e2Tonnes = scanner.nextInt();
@@ -88,10 +88,9 @@ public class CS20SNormalizingExample001{
         // echo e2 weight
         System.out.println("Elephant 2: ");
         System.out.println(e2Tonnes + ":" + e2Kilos + ":" + e2Grams);
-        System.out.println();
-        
+        System.out.println();        
     
-    // **** processing ****
+    // // **** processing ****
     
     //int GRAMSPERTONNE = 1000000;        // grams in one tonne
     //int GRAMSPERKILO = 1000;            // grams in one kilo
@@ -103,7 +102,7 @@ public class CS20SNormalizingExample001{
         e1TotalGrams += e1Grams;
         //System.out.println("e1 total grams: " + e1TotalGrams);
         
-        // e2
+        // // e2
         e2TotalGrams = e2Tonnes * GRAMSPERTONNE;
         e2TotalGrams += e2Kilos * GRAMSPERKILO;
         e2TotalGrams += e2Grams;
@@ -113,10 +112,10 @@ public class CS20SNormalizingExample001{
         totalWeightInGrams = e1TotalGrams + e2TotalGrams;
         //System.out.println("total grams: " + totalWeightInGrams);
         
-        // de-normalize
-        // int totalTonnes = 0;
-        // int totalKilos = 0;
-        // int totalGrams = 0;
+        // // de-normalize
+        // // int totalTonnes = 0;
+        // // int totalKilos = 0;
+        // // int totalGrams = 0;
         
         // get total tonnes
         totalTonnes = totalWeightInGrams / GRAMSPERTONNE;
@@ -124,7 +123,7 @@ public class CS20SNormalizingExample001{
         // re-set total Grams
         totalWeightInGrams = totalWeightInGrams % GRAMSPERTONNE;
         
-        // get total kilos
+        // // get total kilos
         totalKilos = totalWeightInGrams / GRAMSPERKILO;
         
         // re-set total grams
